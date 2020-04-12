4f805aa9655d0210ca77f7fb2a988f40f1cc9094
AngelFFigueroaRivera <angfir@live.com.mx>
2020-04-12 02:35:42 -0500

    review position3

--- mapas_covid.js
+++ mapas_covid.js
@@ -72,6 +72,15 @@ var map = new mapboxgl.Map({
     center: [-101.33083597801148, 22.192387333218626], // starting position [lng, lat]
     zoom: 4.1 // starting zoom
 });
+map.addControl(
+    new mapboxgl.GeolocateControl({
+    positionOptions: {
+    enableHighAccuracy: true
+    },
+    trackUserLocation: true
+    })
+    );
+    
 var loadFiles = [
     d3.json("Mexico_Estados.geojson"),
     d3.csv("contagios.csv")
