c79195d4bc1effd219afe4195ba041aeaf584f48
Mildred Gil <mi.mitel@hotmail.com>
2020-04-14 21:41:53 -0500

    fix

--- mapas_covid.js
+++ mapas_covid.js
@@ -476,8 +476,8 @@ Promise.all(loadFiles).then(function(data) {
         height =    height - margin.top - margin.bottom;
 
         var svg = d3.select('#grafico').append("svg")
-          .attr("width",  width)
-          .attr("height", height)
+          .attr("width",  width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
           .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
