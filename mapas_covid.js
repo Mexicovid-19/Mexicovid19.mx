3a640d0ce6edfa5ede263f90a5b2d642fc556a84
SeaWar741 <juanca741@gmail.com>
2020-04-16 21:58:26 -0500

    added first visitor cookie save and pop up to add to homescreen

--- mapas_covid.js
+++ mapas_covid.js
@@ -969,3 +969,45 @@ function move_config(){
     document.getElementById('config').appendChild(fragment);
     element.style.display = 'none'; 
 }
+
+
+//checar con cookies
+$(document).ready(function () {
+    $("#myModal").click(function(){
+        del_cookie("cookie");   
+    });
+    
+    console.log(document.cookie);
+    //del_cookie("cookie");
+    var visit = getCookie("cookie");
+    if (visit == null) {
+        $("#myModal").modal("show");
+        var expire = new Date();
+        expire = new Date(expire.getTime() + 7776000000);
+        document.cookie = "cookie=here; expires=" + expire;
+    }
+});
+
+function del_cookie(name)
+{
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
